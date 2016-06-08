9132fac0795922431d34dc45b928df7d274a77a5
Diego Vidal <diego@mingadigital.com>
2016-06-08 17:20:25 -0400

    added channel option

--- dvbbc.py
+++ dvbbc.py
@@ -25,7 +25,6 @@ import os,sys,threading,time,argparse
 import socket
 
 
-# LOCAL_IP = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
 HTMLENC = {'"': '&quot;', "'": '&apos'}
 htmlenc = lambda c: escape(c, HTMLENC)
 
