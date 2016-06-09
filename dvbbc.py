a064b732a8c4ed0b88c6624a8e8879a560569c51
Diego Vidal <diego@mingadigital.com>
2016-06-08 20:05:54 -0400

    added channel option

--- dvbbc.py
+++ dvbbc.py
@@ -23,6 +23,8 @@ from wsgiref.validate import validator
 from xml.sax.saxutils import escape
 import os,sys,threading,time,argparse
 import socket
+import time
+
 
 
 LOCAL_IP = [l for l in ([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")][:1], [[(s.connect(('8.8.8.8', 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) if l][0][0]
@@ -180,7 +182,7 @@ def select_channel(channels, value):
 
 def main():
     """Run the show"""
-
+    time.sleep(30) # delays for 5 seconds
     #Arguments parser
     parser = argparse.ArgumentParser(description="Stream live TV over HTTP to multiple viewers, using Sundtek as capture card")
     parser.add_argument("-p","--port",type=int,default=2000,help="server port")
